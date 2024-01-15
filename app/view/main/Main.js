9ae0928b22569c7a276bb9eca1ea26da25f22e84
behzad kazemi <bkazemi1994@gmail.com>
2024-01-15 16:35:09 +0100

    Add scroll bar for harry and product view

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -5,103 +5,111 @@
  *
  * TODO - Replace this content of this view to suite the needs of your application.
  */
-Ext.define('MyApp.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
+Ext.define("MyApp.view.main.Main", {
+  extend: "Ext.tab.Panel",
+  xtype: "app-main",
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
+  requires: [
+    "Ext.plugin.Viewport",
+    "Ext.window.MessageBox",
 
-        'MyApp.view.main.MainController',
-        'MyApp.view.main.MainModel',
-        'MyApp.view.main.List',
-        'MyApp.view.main.ProductView',
-        'MyApp.view.main.MyForm',
-        'MyApp.view.main.HarryPotterCharacterList',
-       
-    ],
+    "MyApp.view.main.MainController",
+    "MyApp.view.main.MainModel",
+    "MyApp.view.main.List",
+    "MyApp.view.main.ProductView",
+    "MyApp.view.main.MyForm",
+    "MyApp.view.main.HarryPotterCharacterList",
+  ],
 
-    controller: 'main',
-    viewModel: 'main',
+  controller: "main",
+  viewModel: "main",
 
-    ui: 'navigation',
+  ui: "navigation",
 
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
+  tabBarHeaderPosition: 1,
+  titleRotation: 0,
+  tabRotation: 0,
 
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
+  header: {
+    layout: {
+      align: "stretchmax",
     },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
+    title: {
+      bind: {
+        text: "{name}",
+      },
+      flex: 0,
     },
+    iconCls: "fa-th-list",
+  },
 
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
+  tabBar: {
+    flex: 1,
+    layout: {
+      align: "stretch",
+      overflowHandler: "none",
+    },
+  },
+
+  responsiveConfig: {
+    tall: {
+      headerPosition: "top",
+    },
+    wide: {
+      headerPosition: "left",
+    },
+  },
+
+  defaults: {
+    bodyPadding: 20,
+    tabConfig: {
+      responsiveConfig: {
         wide: {
-            headerPosition: 'left'
-        }
+          iconAlign: "left",
+          textAlign: "left",
+        },
+        tall: {
+          iconAlign: "top",
+          textAlign: "center",
+          width: 120,
+        },
+      },
     },
+  },
 
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
+  items: [
+    {
+      title: "Home",
+      iconCls: "fa-home",
+      // The following grid shares a store with the classic version's grid as well!
+      items: [{ xtype: "mainlist" }],
     },
+    {
+      title: "Product",
+      iconCls: "fa-user",
+      xtype: "productview",
+      scrollable: true, // Enable scrolling for the panel
+      height: "100%", // Set the height to 80% of the available space
+      width: "100%", // Set the width to 80% of the available space
+    },
+    {
+      title: "TextttSubmit",
+      iconCls: "fa-solid fa-envelope-open-text",
+      xtype: "myform",
+    },
+    {
+      title: "Harry Poter Character",
+      iconCls: "fa-hat-wizard",
 
-    items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{xtype: 'mainlist'},]
-    }, {
-        title: 'Product',
-        iconCls: 'fa-user',
-        items: [{ xtype:'productview'},] ,
-      
-        
-    }, {
-        title: 'TextttSubmit',
-        iconCls: 'fa-solid fa-envelope-open-text',
-        xtype:'myform',
-    }, {
-        title: 'Harry Poter Character',
-        iconCls: 'fa-hat-wizard',
-        
-        items: [{ xtype:'harrypottercharacterlist'},] ,
-        
+      layout: "fit", // Ensure the layout is 'fit'
+      items: [
+        {
+          xtype: "harrypottercharacterlist",
+          scrollable: true, // Enable scrolling for the panel
+          height: "100%", // Set the height to 80% of the available space
+          width: "100%", // Set the width to 80% of the available space
+        },
+      ],
     },
-]
+  ],
 });
-
-
-
